e8a75b244494b1edf79fa8e3a4de8d12521d6c1d
Hikmet Efe Canikli <efsiprocanik@gmail.com>
2023-07-13 09:54:37 +0300

    Data Parsing

--- Assets/Script/WebTest.cs
+++ Assets/Script/WebTest.cs
@@ -1,18 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.Networking;
 public class WebTest : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
+    IEnumerator Start()
     {
-        
+        var www = UnityWebRequest.Get("http://localhost/sqlconnect/webtest.php");
+        yield return www.SendWebRequest();
+        string[] webResults = www.downloadHandler.text.Split("\t");
+        foreach(string res in webResults)
+        {
+            Debug.Log(res);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+   
 }
